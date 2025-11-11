dcdc1ef0521fb702404a093e5cff35eaf12d7d05
abdelhalim <abdelhalim@gmail.com>
2025-11-11 17:57:16 +0200

    ;wrl;awer

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true })); // to parse form data
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // to parse form data
 app.use(cookieParser());
 // app.use(cors({
 //     origin: "https://twitter-ten-rust.vercel.app",
