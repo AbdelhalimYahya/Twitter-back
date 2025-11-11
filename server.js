59c8cb15bb9391ceddc11ee52124c91c80c3d305
abdelhalim <abdelhalim@gmail.com>
2025-11-11 23:12:14 +0200

    asdf

--- server.js
+++ server.js
@@ -41,13 +41,13 @@ app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
   
-//     app.get("*", (req, res) => {
-//       res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//     });
-//   }
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
