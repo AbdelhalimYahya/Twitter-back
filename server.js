432d5787452e2d18fbfd64acdea6eb03d87401e0
abdelhalim <abdelhalim@gmail.com>
2025-11-11 17:29:55 +0200

    asldfkja;sd

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // to parse form data
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: "https://twitter-ten-rust.vercel.app",
     credentials: true,
 }));
 
