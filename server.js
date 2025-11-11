32c81b83d6a878c9f366d4274c3120800cfbf49c
abdelhalim <abdelhalim@gmail.com>
2025-11-11 17:20:29 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import connectMongoDB from "./db/mongoDB.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.route.js";
+import { v2 as cloudinary } from "cloudinary";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import cors from "cors";
+
+dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true })); // to parse form data
+app.use(cookieParser());
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
+
+app.use("/api/auth",authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    connectMongoDB();
+});
