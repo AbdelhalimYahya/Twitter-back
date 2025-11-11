10df63cc8ff06d5f36b683bfaf3b7c754f5b8ed0
abdelhalim <abdelhalim@gmail.com>
2025-11-11 21:44:45 +0200

    asdfa

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,11 +5,19 @@ export const generateTokenAndSetCookie = (userId, res) => {
 		expiresIn: "15d",
 	});
 
-	res.cookie("jwt", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000, //MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "none", // CSRF attacks cross-site request forgery attacks
-		secure: true,
-		partitioned: true,
+	res.cookie("token", token, {
+		httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24 * 30,
+        partitioned: true,
 	});
+
+	return token;
 };
+
+// maxAge: 15 * 24 * 60 * 60 * 1000, //MS
+// httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+// sameSite: "none", // CSRF attacks cross-site request forgery attacks
+// secure: true,
+// partitioned: true,
