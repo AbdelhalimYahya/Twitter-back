8b0646b432b9444e5cdc08ea6f9fae4da2f2eb57
abdelhalim <abdelhalim@gmail.com>
2025-11-11 21:25:27 +0200

    asdfl

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(express.json({ limit: "50mb" }));
-// app.use(express.urlencoded({ limit: "50mb", extended: true })); // to parse form data
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // to parse form data
 app.use(cookieParser());
 app.use(cors({
     origin: "https://twitter-ten-rust.vercel.app",
